75d69dca2c6bf621e832e343abf75bcb2f783139
Esteban Sayago <sayagoo823@gmail.com>
2024-02-22 01:06:47 -0300

    update files

--- public/script.js
+++ public/script.js
@@ -1,33 +1,101 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const selectInput = document.getElementById("provincia");
-  const localidad = document.getElementById("localidad");
-  fetch("/provincias")
-    .then((res) => res.json())
-    .then((data) =>
-      data.provincias
-        .sort((a, b) => a.nombre.localeCompare(b.nombre))
-        .forEach((provincia) => {
-          selectInput.innerHTML += `
-  <option value=${provincia.nombre}>${provincia.nombre}</option>
-  `;
-        }),
-    );
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
 
-  selectInput.addEventListener("change", setLocalidad);
+nextBtnFirst.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function () {
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function () {
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  }, 800);
+});
 
-  function setLocalidad() {
-    localidad.innerHTML = "";
-    const provinciaValue = selectInput.value;
-    fetch("/departamentos/" + provinciaValue)
-      .then((res) => res.json())
-      .then((json) =>
-        json.departamentos
-          .sort((a, b) => a.nombre.localeCompare(b.nombre))
-          .forEach((dep) => {
-            localidad.innerHTML += `
-      <option value=${dep.nombre}> ${dep.nombre} </option>
-      `;
-          }),
-      );
-  }
+prevBtnSec.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+const sortEntities = (arr) => {
+  const sorted = [...arr];
+  sorted.sort((a, b) => a.nombre.localeCompare(b.nombre));
+  return sorted;
+};
+
+const { provincias } = await fetch("/provincias").then((res) => res.json());
+const sorted = sortEntities(provincias);
+
+const prInputs = document.querySelectorAll(".pr");
+const scInputs = document.querySelectorAll(".sc");
+console.log(scInputs);
+
+prInputs.forEach((inp) => {
+  const id = inp.getAttribute("id").split("-")[1];
+  inp.addEventListener("change", async () => {
+    const { departamentos } = await fetch("/departamentos/" + inp.value).then((res) => res.json());
+    const sc = id === "o" ? scInputs[0] : scInputs[1];
+    sc.innerHTML = "";
+    const sortedDep = sortEntities(departamentos);
+    sortedDep.forEach((dp) => {
+      sc.innerHTML += `<option value=${dp.nombre}>${dp.nombre}</option>`;
+    });
+  });
+  sorted.forEach((pr) => {
+    inp.innerHTML += `<option value=${pr.nombre}>${pr.nombre}</option>`;
+  });
 });
